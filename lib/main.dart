d22860ff256d5ab126a2e544957056cce3a07e0c
krish7x <krishnakumar99g@gmail.com>
2021-03-29 16:33:25 +0530

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final word = WordPair.random();
     return MaterialApp(
       title: "First App By learning through docs",
       home: Scaffold(
@@ -14,7 +12,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome'),
         ),
         body: Center(
-          child: Text(word.asPascalCase),
+          child: Text('Hello World!'),
         ),
       ),
     );
