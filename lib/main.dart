2892297964240a2dda4580fca98d33fa173f6655
krish7x <krishnakumar99g@gmail.com>
2021-03-29 16:32:10 +0530

    statefull

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,27 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final word = WordPair.random();
     return MaterialApp(
       title: "First App By learning through docs",
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome'),
         ),
-        body: Center(
-          child: Text(word.asPascalCase),
-        ),
+        body: Center(child: RandomWords()),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final word = WordPair.random();
+    return Text(word.asPascalCase);
   }
 }
